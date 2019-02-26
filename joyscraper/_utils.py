c69c4e1b80e6d7082ce9e2e364394f3f1342fcb3
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-26 16:54:10 +0000

    Refactored Utils

--- joyscraper/_utils.py
+++ joyscraper/_utils.py
@@ -8,19 +8,3 @@ def getImageFromElementCSS(element):
         return matches[0].split('(')[1].split(")")[0]
     else:
         return ''
-
-
-def parseArticle(articles):
-    result = []
-    for article in articles:
-        data = {}
-        data['link'] = article.div.find('a')['href']
-        data['title'] = article.div.find('h2', class_="entry-title").text
-        category = article.find('div', class_="entry-category")
-        if category:
-            data['category'] = category.text
-        else:
-            data['category'] = 'Main Headline'
-        # data['image'] = getImageFromElementCSS(article.div['style'])
-        result.append(data)
-    return result
